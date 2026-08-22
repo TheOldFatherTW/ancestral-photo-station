18428902ce436f8318c62e89b517576a5e6715bd
TheOldFatherTW <weslie0705@gmail.com>
2026-08-22 10:49:55 +0800

    Point the public door at the current vault tunnel.

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-window.VAULT_ORIGIN = "https://representations-ways-kijiji-weeks.trycloudflare.com";
+window.VAULT_ORIGIN = "https://lift-expense-grown-funny.trycloudflare.com";
 window.VAULT_STATUS_URL = window.VAULT_ORIGIN + "/api/public";
